1402046937a3e113bc14e16f558f33e387e8f1f4
ChineseWong <wongb03@gmail.com>
2026-02-24 08:52:48 -0500

    Add files via upload

---
+++ sw-4.js
@@ -0,0 +1,21 @@
+// Self-destruct: clear caches and unregister
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(names.map(function(name) { return caches.delete(name); }));
+    }).then(function() {
+      return self.registration.unregister();
+    }).then(function() {
+      return self.clients.matchAll();
+    }).then(function(clients) {
+      clients.forEach(function(client) { client.navigate(client.url); });
+    })
+  );
+});
+// Don't intercept any fetches - let them go to network
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request));
+});
